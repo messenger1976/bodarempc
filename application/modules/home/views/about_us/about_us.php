0f65def1c623b409f201c7bfb1b85ab491c93833
Herald Felisilda <herald_felisilda@yahoo.com>
2026-01-19 18:38:00 +0800

    Update Added About Us Page

--- application/modules/home/views/about_us/about_us.php
+++ application/modules/home/views/about_us/about_us.php
@@ -1,5 +1,5 @@
     <!-- Page Header Start -->
-    <div class="container-fluid page-header py-5 mb-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container-fluid py-5 mb-5 wow fadeIn" data-wow-delay="0.1s" style="background: linear-gradient(to right, rgb(2, 36, 91) 0%, rgba(2, 36, 91, 0) 100%), url(../themes/bodare/website/assets/img/members-training.jpg) center center no-repeat">
         <div class="container py-5">
             <h1 class="display-3 text-white animated slideInRight">About Us</h1>
             <nav aria-label="breadcrumb">
